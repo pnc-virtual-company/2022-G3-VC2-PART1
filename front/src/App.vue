9ac2174cf4db7477a54a95f99d2b7edfd40cd39e
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-08-21 09:42:07 +0700

    send mail#8

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<TheNavigation/>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> 
@@ -7,15 +8,16 @@
 </template>
 
 <style>
-body{
-  background: rgba(233, 233, 233, 0.747);
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-<TheNavigation></TheNavigation>
+  body{
+    background: rgba(233, 233, 233, 0.747);
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    }
+</style>
 <script>
 import TheNavigation from '@/components/navigation/TheNavigation.vue';
  

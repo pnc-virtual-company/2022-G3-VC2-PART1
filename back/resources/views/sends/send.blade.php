84743c210b72ed336bdf42f90f56fb0f750a1967
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-08-22 10:10:57 +0700

    email#8

--- back/resources/views/sends/send.blade.php
+++ back/resources/views/sends/send.blade.php
@@ -1,5 +1,6 @@
 <p>Dear Social Affair team. <br> </p>
-<p>My name is {{$username}} I would like to ask permission for {{$leaveType}} for {{$details}} <br>
+<p>My name is {{$username}} I would like to ask permission for {{$leaveType}} <br>
+    for {{$details}} from {{$start_date}} to {{$end_date}} for {{$days}} days <br>
     Email: {{$email}}
 </p>
 <b>Best regard <br>

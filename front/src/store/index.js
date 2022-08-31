df089de07b14a912764f940ebe7864a7d31892eb
keanhor2 <keanhor2022@gmail.com>
2022-08-31 10:50:20 +0700

    Merge branch 'main' of https://github.com/pnc-virtual-company/2022-G3-VC2-PART1

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -3,10 +3,8 @@ import { defineStore } from 'pinia'
 export const dataStore = defineStore('user_login', {
   state: () => ({ 
     login: false ,
-<<<<<<< HEAD
     user: "Khy"
-=======
->>>>>>> 7ed07e5d101118c6eea0627466fea0ffe6bdb09f
+
   }),
   // getters: {
   //   double: state => state.count * 2,
